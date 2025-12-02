1d70343253443fc9e77e3668019c4e62a9d910c3
chengweiyong <cwyong-wm23@student.tarc.edu.my>
2025-12-02 16:54:48 +0800

    Added editing function where it finds the product by ID and loads the view with the existing data.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,7 +15,7 @@ class ProductController extends Controller
 
     public function __construct()
     {
-        // Wrap base service with image decorato
+        // Wrap base service with image decorator
         $this->service = new ProductImageDecorator(new BaseProductService());
     }
 
